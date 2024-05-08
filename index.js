04da168d1cca647f1d035d15e518b88ba32ec045
fasfasf2sdg <gs1jrdo0831@hotmail.com>
2024-05-08 15:22:43 +0700

    feat

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function largestNumber(nums) {
-  if (nums.every((num) => num === 0)) return "0";
-  return nums.sort((a, b) => `${b}${a}` - `${a}${b}`).join("");
+function isValidBST(root) {
+  return isValid(root, null, null);
+}
+function isValid(node, min, max) {
+  if (!node) return true;
+  if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+    return false;
+  return (
+    isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+  );
 }
