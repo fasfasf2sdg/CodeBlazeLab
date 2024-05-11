2a3f9bd20457c2813e73ce4bacf4f2a32af53983
fasfasf2sdg <gs1jrdo0831@hotmail.com>
2024-05-11 11:08:21 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function singleNumber(nums) {
-  let result = 0;
-  for (const num of nums) {
-    result ^= num;
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    let j = 0;
+    while (
+      j < prefix.length &&
+      j < strs[i].length &&
+      prefix.charAt(j) === strs[i].charAt(j)
+    ) {
+      j++;
+    }
+    prefix = prefix.substring(0, j);
   }
-  return result;
+  return prefix;
 }
